2576029fb244fdc8406e5f0084f2185bfa04a64e
liwenwen <https://gitee.com/Li_W/the_ten_power.git>
2019-02-18 23:03:19 +0800

    查询和分页查询

--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,10 +3,19 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -36,8 +45,60 @@ public class LabelService {
         labelDao.save(label);
     }
 
-    public void deleteById(String id){
+    public void deleteById(String id) {
         labelDao.deleteById(id);
     }
 
+    public List<Label> findSearch(Label label) {
+        return labelDao.findAll(new Specification<Label>() {
+            /**
+             *
+             * @param root 跟对象
+             * @param criteriaQuery 封装的查询关键字
+             * @param criteriaBuilder 同来封装条件对象
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                //list存放所有条件
+                List<Predicate> list = new ArrayList<>();
+                if (label.getLabelname() != null && !"".equals(label.getLabelname())) {
+                    Predicate predicate = criteriaBuilder.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%"); //where like labelname = %小明%
+                    list.add(predicate);
+                }
+                if (label.getState() != null && !"".equals(label.getState())) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("state").as(String.class), label.getState()); //where state = "1"
+                    list.add(predicate);
+                }
+                return criteriaBuilder.and(list.toArray(new Predicate[list.size()])); //where  labelname = %小明% and state = "1"
+            }
+        });
+    }
+
+    public Page<Label> pageQuery(Label label, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return labelDao.findAll(new Specification<Label>() {
+            /**
+             *
+             * @param root 跟对象
+             * @param criteriaQuery 封装的查询关键字
+             * @param criteriaBuilder 同来封装条件对象
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                //list存放所有条件
+                List<Predicate> list = new ArrayList<>();
+                if (label.getLabelname() != null && !"".equals(label.getLabelname())) {
+                    Predicate predicate = criteriaBuilder.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%"); //where like labelname = %小明%
+                    list.add(predicate);
+                }
+                if (label.getState() != null && !"".equals(label.getState())) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("state").as(String.class), label.getState()); //where state = "1"
+                    list.add(predicate);
+                }
+                return criteriaBuilder.and(list.toArray(new Predicate[list.size()])); //where  labelname = %小明% and state = "1"
+            }
+        }, pageable);
+    }
 }
